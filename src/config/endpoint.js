afa19737c9cdef6dd553196ae9b1d7fbb8664ca6
Pradeep nishani <pradeep.nishani@gmail.com>
2018-04-09 00:36:54 +0530

    second version

---
+++ src/config/endpoint.js
@@ -0,0 +1,1 @@
+export const relayApi='https://api.graph.cool/relay/v1/cjfqxziag0o4u0153uszcqnvq'
---
+++ src/containers/Template.js
@@ -0,0 +1,20 @@
+import React,{Component} from 'react'
+
+class Template extends Component {
+    render(){
+
+        return(
+            <div>
+                <header>
+                    <h1> Tic Tac</h1>
+                </header>
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+
+        )
+    }
+}
+
+export default Template
